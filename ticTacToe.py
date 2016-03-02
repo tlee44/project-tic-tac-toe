2bebe1229f6debb485b7227bbded9fef0a323d76
Tyler Lee <tlee44@mail.valenciacollege.edu>
2016-03-02 13:24:33 -0500

    Tic Tac Toe
    Woah this was tough. I had no idea where to start with the CheckWinner
    section. Had to seek additional help. Hopefully it works.

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,25 +1,48 @@
 def printBoard(board):
-    # TO DO #################################################################
-    # Write code in this function that prints the game board.               #
-    # The code in this function should only print, the user should NOT      #
-    # interact with this function in any way.                               #
-    #                                                                       #
-    # Hint: you can follow the same process that was done in the textbook.  #
-    #########################################################################
-
-def checkWinner(board, player):    
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ', 'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': ' ' }
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+printBoard(theBoard)
+def checkWinner(board, player):
     print('Checking if ' + player + ' is a winner...')
-    
-    # TO DO #################################################################
-    # Write code in this function that checks the tic-tac-toe board          #
-    # to determine if the player stored in variable 'player' currently      #
-    # has a winning position on the board.                                  #
-    # This function should return True if the player specified in           #
-    # variable 'player' has won. The function should return False           #
-    # if the player in the variable 'player' has not won.                   #
-    #########################################################################
-    
-    
+    if board['top-L'] == ('X') and board['top-M'] == ('X') and board['top-R'] ==('X'):
+        return True
+    if board['top-L'] == ('O') and board['top-M'] == ('O') and board['top-R'] ==('O'):
+        return True
+    if board['mid-L'] == ('X') and board['mid-M'] == ('X') and board['mid-R'] ==('X'):
+        return True
+    if board['mid-L'] == ('O') and board['mid-M'] == ('O') and board['mid-R'] ==('O'):
+        return True
+    if board['low-L'] == ('X') and board['low-M'] == ('X') and board['low-R'] ==('X'):
+        return True
+    if board['low-L'] == ('O') and board['low-M'] == ('O') and board['low-R'] ==('O'):
+        return True
+    if board['low-L'] == ('X') and board['mid-M'] == ('X') and board['top-R'] ==('X'):
+        return True
+    if board['low-L'] == ('O') and board['mid-M'] == ('O') and board['top-R'] ==('O'):
+        return True
+    if board['top-L'] == ('O') and board['mid-M'] == ('O') and board['low-R'] ==('O'):
+        return True
+    if board['top-L'] == ('X') and board['mid-M'] == ('X') and board['low-R'] ==('X'):
+        return True
+    if board['low-L'] == ('X') and board['mid-L'] == ('X') and board['top-L'] ==('X'):
+        return True
+    if board['low-M'] == ('X') and board['mid-M'] == ('X') and board['top-M'] ==('X'):
+        return True
+    if board['low-R'] == ('X') and board['mid-R'] == ('X') and board['top-R'] ==('X'):
+        return True
+    if board['low-L'] == ('O') and board['mid-L'] == ('O') and board['top-L'] ==('O'):
+        return True
+    if board['low-M'] == ('O') and board['mid-M'] == ('O') and board['top-M'] ==('O'):
+        return True
+    if board['low-R'] == ('O') and board['mid-R'] == ('O') and board['top-R'] ==('O'):
+        return True
+    else
+        return False
+    #Hope ^^^ is correct. Was super confused as to how to checkWinner
 def startGame(startingPlayer, board):
     # TO DO #################################################################
     # Add comments to each line in this function to describe what           #
@@ -27,22 +50,28 @@ def startGame(startingPlayer, board):
     #########################################################################
 
     turn = startingPlayer
+    #Creates variable turn equal to startingPlayer
     for i in range(9):
         printBoard(board)
+    #Prints board at start of new turn
         print('Turn for ' + turn + '. Move on which space?')
         move = input()
+    #Asks for player's move
         board[move] = turn
+    #Equates move to turn
         if( checkWinner(board, 'X') ):
             print('X wins!')
             break
+    #Checks to see if X has won
         elif ( checkWinner(board, 'O') ):
             print('O wins!')
             break
-    
+    #Checks to see if O has won
         if turn == 'X':
             turn = 'O'
         else:
             turn = 'X'
-        
+    #Changes the current player
+
     printBoard(board)
-    
+    #Prints board with updated moves
